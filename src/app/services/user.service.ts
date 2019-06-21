e2f50af25c76f69a0b557007d1e7e0e5477841f1
jainudit012 <jainudit.012@gmail.com>
2019-06-21 19:01:07 +0530

    add auth service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt'
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import Axios from 'axios';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  helper:JwtHelperService
+
+  constructor(private http: HttpClient) { 
+    this.helper = new JwtHelperService()
+  }
+
+  public async getToken(token:string){
+    const data = {
+      token: token
+    }
+    return Axios.post(`${environment.apiUrl}/users`, data).then((res:any)=>{
+      return res
+    }).catch((err:any)=> {return err})
+    // return this.http.post(`${environment.apiUrl}/users`, data).subscribe()
+  }
+
+  public decodeToken(token:string){
+    // const helper = new JwtHelperService()
+    // return helper.decodeToken(token)
+    return this.helper.decodeToken(token)
+  }
+
+  public editUser(token:string){
+  }
 }
