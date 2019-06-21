f0dd2f2b7578fc4566e89a9aa0856acf9e086c82
jainudit012 <jainudit.012@gmail.com>
2019-06-21 19:02:32 +0530

    nav component

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { print } from '../../app.component';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-navbar',
+  selector: 'navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  activeRouteString = ''
+
+  constructor(private authService: AuthService,
+    private router: Router) { 
+      this.router.events.subscribe(()=>this.activeRouteString = router.url)
+    }
 
   ngOnInit() {
+  }
+
+  login() {
+    print('login button tap')
+    this.authService.login()
+  }
+
+  logout(){
+    print('logout button tap')
+    this.authService.logout()
+    // this.store.dispatch( new Logout())
   }
 
 }
