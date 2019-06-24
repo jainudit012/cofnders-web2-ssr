1dbafa41779ed8bbd88b916fa2468b53fdcc3ec4
jainudit012 <jainudit.012@gmail.com>
2019-06-24 18:00:53 +0530

    add auth user model

---
+++ src/app/models/auth-user.model.ts
@@ -0,0 +1,17 @@
+export class AuthUser {
+    given_name: string;
+    family_name: string;
+    nickname: string;
+    name: string;
+    picture: string;
+    updated_at: Date;
+    email: string;
+    emil_verified: boolean;
+    iss: string;
+    sub: string;
+    aud: string;
+    iat: Date;
+    exp: Date;
+    at_hash: string;
+    nonce: string;
+}
