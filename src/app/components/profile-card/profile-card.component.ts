1b563ce716d49563d27e705dcc20559a72737c35
jainudit012 <jainudit.012@gmail.com>
2019-06-26 19:39:10 +0530

    profile card component

--- src/app/components/profile-card/profile-card.component.ts
+++ src/app/components/profile-card/profile-card.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'app-profile-card',
+  selector: 'profile-card',
   templateUrl: './profile-card.component.html',
   styleUrls: ['./profile-card.component.scss']
 })
-export class ProfileCardComponent implements OnInit {
+export class ProfileCardComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  appUserRes$:any
+
+  constructor(private userService: UserService, 
+    private authService: AuthService) {
+
+      if(this.authService.isAuthenticated()&&this.authService.isUserAuthenticated()){
+        this.appUserRes$ = this.userService.getUser(this.authService.userToken)
+      }
+    }
 
   ngOnInit() {
+
   }
+
+  ngOnDestroy(){
+  }
+
+  login(){
+    this.authService.login()
+  }
+
+  
 
 }
