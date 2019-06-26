5a02fad26c7b35c68e559bbbb5cb344dc0153eaa
jainudit012 <jainudit.012@gmail.com>
2019-06-26 19:40:22 +0530

    add mat card module

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -6,6 +6,7 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import {MatSelectModule} from '@angular/material/select';
 import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   imports: [
@@ -15,7 +16,8 @@ import {MatInputModule} from '@angular/material/input';
     MatSnackBarModule,
     MatMenuModule,
     MatSelectModule,
-    MatInputModule
+    MatInputModule,
+    MatCardModule
   ],
   exports: [
     MatProgressSpinnerModule,
@@ -23,7 +25,8 @@ import {MatInputModule} from '@angular/material/input';
     MatSnackBarModule,
     MatMenuModule,
     MatSelectModule,
-    MatInputModule
+    MatInputModule,
+    MatCardModule
   ]
 })
 export class MaterialModule { }
