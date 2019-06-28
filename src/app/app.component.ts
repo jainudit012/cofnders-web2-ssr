5fde650acd73df8f8e350b201954fa279bea20ac
jainudit012 <jainudit.012@gmail.com>
2019-06-28 13:56:44 +0530

    fix callback profile component rendering

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { environment } from '../environments/environment';
 import { AuthService } from './services/auth.service';
 import { UserService } from './services/user.service';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'cofnders-web2-ssr';
   url: string
 
   constructor(private authService: AuthService,
     private userService: UserService,
-    private router: Router){ 
-      this.router.events.subscribe(()=> {
-        this.url = this.router.url 
-      });
+    public router: Router){ 
   }
 
   ngOnInit(){
