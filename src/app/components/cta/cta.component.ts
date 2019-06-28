20c204d64170c7739387d8c6352650d9cacabe91
jainudit012 <jainudit.012@gmail.com>
2019-06-28 16:13:39 +0530

    cta component without create new resource

--- src/app/components/cta/cta.component.ts
+++ src/app/components/cta/cta.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
-  selector: 'app-cta',
+  selector: 'cta',
   templateUrl: './cta.component.html',
   styleUrls: ['./cta.component.scss']
 })
 export class CtaComponent implements OnInit {
 
-  constructor() { }
+  constructor(public router: Router,
+    private authService: AuthService) { }
 
   ngOnInit() {
+  }
+
+  createProject(){
+    if(this.authService.isAuthenticated()&&this.authService.isUserAuthenticated()){
+      console.log('create project tap')
+    }else{
+      this.authService.login()
+    }    
+  }
+
+  createOpportunity(){
+    if(this.authService.isAuthenticated()&&this.authService.isUserAuthenticated()){
+      console.log('create opportunity tap')
+    }else{
+      this.authService.login()
+    }
+  }
+
+  listFund(){
+    if(this.authService.isAuthenticated()&&this.authService.isUserAuthenticated()){
+      console.log('list fund tap')
+    }else{
+      this.authService.login()
+    }
+  }
+
+  routeIdeas(){
+    this.router.navigate(['/ideas'])
   }
 
 }
