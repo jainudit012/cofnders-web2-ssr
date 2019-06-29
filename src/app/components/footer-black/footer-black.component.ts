ca7baeb43bb133103e1f0e2e6b9c1686f63dd5b1
jainudit012 <jainudit.012@gmail.com>
2019-06-29 14:36:07 +0530

    footer black component

---
+++ src/app/components/footer-black/footer-black.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'footer-black',
+  templateUrl: './footer-black.component.html',
+  styleUrls: ['./footer-black.component.scss']
+})
+export class FooterBlackComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
