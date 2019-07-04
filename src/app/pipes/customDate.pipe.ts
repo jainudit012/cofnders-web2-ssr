1db26048ca14bfa1b357bf1c0df4d30df26d3769
jainudit012 <jainudit.012@gmail.com>
2019-07-04 19:57:39 +0530

    get date from now pipe

---
+++ src/app/pipes/customDate.pipe.ts
@@ -0,0 +1,11 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import * as moment from 'moment'
+
+
+@Pipe({name: 'dateFromNow'}) 
+export class CustomDatePipe implements PipeTransform {
+    transform(value: Date, args?:any){
+        let newDate: string = moment(value).fromNow()
+        return newDate;
+      }
+}
