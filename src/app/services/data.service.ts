2f0b9b7ca017467402d2de24d4efc891f9b0b06a
jainudit012 <jainudit.012@gmail.com>
2019-07-04 20:01:15 +0530

    get feed data

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -4,13 +4,15 @@ import Axios from 'axios';
 
 import { environment } from '../../environments/environment';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor(private _snackBar: MatSnackBar) { }
+  constructor(private _snackBar: MatSnackBar,
+    private authService: AuthService) { }
 
   public getRecent(){
     return Axios.get(`${environment.apiUrl}/ideas`).then(res=>{
@@ -19,5 +21,24 @@ export class DataService {
       if(!environment.production) console.log(err)
       this._snackBar.open('Could not Load Recent Ideas!', 'X', {duration: 2000})
     })
+  }
+
+  public getFeed(){
+    if(this.authService.isAuthenticated() && this.authService.isUserAuthenticated()){
+      const token = this.authService.userToken
+      return Axios.get(`${environment.apiUrl}/feed`, {headers: {'Authorization': token}}).then(res=>{
+        return res.data
+      }).catch(err=>{
+        if(!environment.production) console.log(err)
+        this._snackBar.open('Could not Your Feed!', 'X', {duration: 2000})
+      })
+    }else {
+      return Axios.get(`${environment.apiUrl}/feed`).then(res=>{
+        return res.data
+      }).catch(err=>{
+        if(!environment.production) console.log(err)
+        this._snackBar.open('Could not Your Feed!', 'X', {duration: 2000})
+      })
+    }
   }
 }
