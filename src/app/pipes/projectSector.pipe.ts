afc9dea9efc2baa169ff1186dc663f38918bc2b3
jainudit012 <jainudit.012@gmail.com>
2019-07-04 19:58:12 +0530

    transform sector pipe

---
+++ src/app/pipes/projectSector.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Sector } from '../models/project.model';
+
+
+@Pipe({name: 'sector'}) 
+export class ProjectSectorPipe implements PipeTransform {
+    transform(value: string|number, args?:any){
+        if(value in Sector){
+            return Sector[value]
+        }else return value
+    }
+}
