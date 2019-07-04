c63257b0dcbf25b03229c9fa17cfd2537aed2791
jainudit012 <jainudit.012@gmail.com>
2019-07-04 19:57:30 +0530

    add enums

--- src/app/models/project.model.ts
+++ src/app/models/project.model.ts
@@ -1,3 +1,38 @@
 export class Project {
     
 }
+
+export enum TeamSizes {
+    SINGLE = "1",
+    VERY_SMALL = "2-3",
+    SMALL = "4-10",
+    TEN_PLUS = "11-20",
+    TWENTY_PLUS = "21-50",
+    MEDIUM = "51-100",
+    LARGE = "100+"
+}
+
+export enum ProjectNature {
+    PRODUCT = 'Product',
+    SERVICE = 'Service',
+    CAUSE = 'Cause',
+    OTHERS = 'Others'
+}
+
+export enum Sector {
+HEALTHCARE_FITNESS= 'Healthcare and Fitness',
+FIN_TECH= 'FinTech',
+E_COMMERCE= 'Ecommerce',
+CONSUMER= 'Consumer',
+FOOD= 'Food',
+EDUCATION= 'Education',
+HR= 'HR and Talent platform'
+}
+
+export enum StartupStage {
+IDEA = 'Idea',
+MVP = 'MVP',
+GROWTH= 'Growth',
+BETA = 'Beta',
+GROWING = 'Growing in Revenues'
+}
