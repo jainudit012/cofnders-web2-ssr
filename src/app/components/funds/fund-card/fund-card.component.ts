8990af965f75c4b0ab47b104631a44208134f68d
jainudit012 <jainudit.012@gmail.com>
2019-07-06 19:01:54 +0530

    add fund card functionality

--- src/app/components/funds/fund-card/fund-card.component.ts
+++ src/app/components/funds/fund-card/fund-card.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { AuthService } from '../../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-fund-card',
+  selector: 'fund-card',
   templateUrl: './fund-card.component.html',
   styleUrls: ['./fund-card.component.scss']
 })
 export class FundCardComponent implements OnInit {
 
-  constructor() { }
+  @Input('fund') fundData
+
+  constructor(public authService: AuthService,
+    private router: Router) { }
 
   ngOnInit() {
+  }
+
+  apply() {
+    if(this.authService.isAuthenticated() && this.authService.isUserAuthenticated()){
+
+    }else {
+      this.authService.login(this.router.url)
+    }
+    console.log('apply tapped')
   }
 
 }
