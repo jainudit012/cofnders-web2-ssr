db16ad534281c306feac3ba06826bf5fb5dddcff
jainudit012 <jainudit.012@gmail.com>
2019-07-06 16:13:06 +0530

    add enum Requirement

--- src/app/models/opportunity.model.ts
+++ src/app/models/opportunity.model.ts
@@ -1,3 +1,12 @@
+export enum Requirement {
+    IDEA_VALIDATION= 'Idea Validation',
+    CO_FOUNDER= 'Co Founder',
+    SERVICE_PARTNERS_VENDORS= 'Service Partners/Vendors',
+    REFERENCE_CONNECTIONS= 'Reference/ Connections',
+    MENTORS_CONSULTANTS = 'Mentors/Consultants',
+    FUNDING = 'Funding'
+}
+
 export class Opportunity {
     
 }
