b1c025c0e4f2e018ff59f7637a8f579dc6fe611a
jainudit012 <jainudit.012@gmail.com>
2019-07-06 16:10:07 +0530

    pipe to transform Opportunity Requirement data

---
+++ src/app/pipes/opportunityRequirement.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Requirement } from '../models/opportunity.model';
+
+
+@Pipe({name: 'requirement'}) 
+export class OpportunityRequirementPipe implements PipeTransform {
+    transform(value: string|number, args?:any){
+        if(value in Requirement){
+            return Requirement[value]
+        }else return value
+    }
+}
