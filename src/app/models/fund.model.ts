4c03545ff8d1d74efac5bb94383bf740565bd4a9
jainudit012 <jainudit.012@gmail.com>
2019-07-06 19:01:03 +0530

    add enum investor type

--- src/app/models/fund.model.ts
+++ src/app/models/fund.model.ts
@@ -1,3 +1,10 @@
 export class Fund {
     
 }
+
+export enum InvestorType {
+    INDIVIDUAL_INVESTOR= 'Individual Investor',
+    INCUBATOR= 'Incubator',
+    FAMILY= 'Family Office',
+    ANGEL_NETWORK= 'Angel Network'
+  }
