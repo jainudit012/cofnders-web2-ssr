a69b3319c96d967c8599b2ade1922e5e60f10744
jainudit012 <jainudit.012@gmail.com>
2019-07-06 18:59:33 +0530

    investor type pipe transform

---
+++ src/app/pipes/investorType.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { InvestorType } from '../models/fund.model';
+
+
+@Pipe({name: 'investorType'}) 
+export class InvestorTypePipe implements PipeTransform {
+    transform(value: string|number, args?:any){
+        if(value in InvestorType){
+            return InvestorType[value]
+        }else return value
+    }
+}
