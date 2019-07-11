d0686b7f333edcb37c4d25dba5a6030a2841b3a5
jainudit012 <jainudit.012@gmail.com>
2019-07-11 16:10:04 +0530

    add create project

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -6,6 +6,8 @@ import { environment } from '../../environments/environment';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { AuthService } from './auth.service';
 
+import { ProjectNature, Sector, StartupStage, TeamSizes } from '../models/project.model';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -57,6 +59,30 @@ export class DataService {
       }).catch(err=>{
         if(!environment.production) console.log(err)
         this._snackBar.open('Could not Load Funds!', 'X', {duration: 2000})
+      })
+    }
+  }
+
+  public createProject(data:any){
+    if(this.authService.isAuthenticated() && this.authService.isUserAuthenticated()){
+      const token = this.authService.userToken
+      const projectData = {
+        ...data.projectBasic,
+        ...data.projectDetails,
+        ...data.projectFinishing
+      }
+      projectData.category = Object.keys(ProjectNature).find(k=> ProjectNature[k]=== data.projectBasic.category)
+      projectData.sector = Object.keys(Sector).find(k=> Sector[k]=== data.projectBasic.sector)
+      projectData.stage = Object.keys(StartupStage).find(k=> StartupStage[k]=== data.projectBasic.stage)
+      projectData.teamSize = Object.keys(TeamSizes).find(k=> TeamSizes[k]=== data.projectBasic.teamSize)
+      if(!environment.production) console.log(projectData)
+
+      Axios.post(`${environment.apiUrl}/projects`, projectData ,{headers: {'Authorization':token}}).then(res=>{
+        this._snackBar.open('Successfully Created the Project', 'X', {duration: 2000})
+        if(!environment.production) console.log(res.data)
+      }).catch(err=>{
+        this._snackBar.open('Could not create the Project', 'X', {duration: 2000})
+        if(!environment.production) console.log('Error response data',err.response.data)
       })
     }
   }
