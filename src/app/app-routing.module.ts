f0fed52453e28187b6d68f6e3014085fcdc0b110
jainudit012 <jainudit.012@gmail.com>
2019-07-15 19:06:37 +0530

    remove unnnecssary imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,13 +8,6 @@ import { IdeaComponent } from './components/ideas/idea/idea.component';
 import { PrivacyPageComponent } from './components/privacy-page/privacy-page.component';
 import { TermsPageComponent } from './components/terms-page/terms-page.component';
 import { TrustPageComponent } from './components/trust-page/trust-page.component';
-import { PostSignUpFormComponent } from './components/forms/post-sign-up-form/post-sign-up-form.component';
-import { ProfileCardComponent } from './components/profile-card/profile-card.component';
-import { CtaComponent } from './components/cta/cta.component';
-import { RecentCardComponent } from './components/recent-card/recent-card.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { FooterBlackComponent } from './components/footer-black/footer-black.component';
-import { ProjectFormComponent } from './components/ideas/project-form/project-form.component';
 
 const routes: Routes = [
   {
@@ -43,9 +36,6 @@ const routes: Routes = [
 
   {
     path: 'trust', component: TrustPageComponent
-  },
-  {
-    path: 'form', component: ProjectFormComponent
   },
 
   {
