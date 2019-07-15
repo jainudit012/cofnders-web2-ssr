d1f96ceaf7ba4f9b1c01ddf738eef89c44ccbf81
jainudit012 <jainudit.012@gmail.com>
2019-07-15 19:00:41 +0530

    list fund form component

--- src/app/components/funds/list-fund-form/list-fund-form.component.ts
+++ src/app/components/funds/list-fund-form/list-fund-form.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Optional, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { InvestorType } from '../../../models/fund.model';
+import { StartupStage } from '../../../models/project.model';
+import { DataService } from '../../../services/data.service';
 
 @Component({
   selector: 'app-list-fund-form',
@@ -10,15 +14,57 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
   ]
 })
 export class ListFundFormComponent implements OnInit {
+  investorTypes: string[]
+  startupStages : string []
+
+  form = new FormGroup({
+    investorType: new FormControl('', Validators.required),
+    name: new FormControl('', Validators.required),
+    contact: new FormControl('', Validators.required),
+    linkedInLink: new FormControl('', Validators.required),
+    startupStage: new FormControl('', Validators.required)
+  })
 
   constructor(public dialogRef: MatDialogRef<ListFundFormComponent>,
-    @Optional() @Inject(MAT_DIALOG_DATA) private data: any) { }
+    @Optional() @Inject(MAT_DIALOG_DATA) private data: any,
+    public dataService: DataService) { }
 
   ngOnInit() {
+    this.investorTypes = [InvestorType.ANGEL_NETWORK, InvestorType.FAMILY, InvestorType.INCUBATOR, InvestorType.INDIVIDUAL_INVESTOR]
+    this.startupStages = [StartupStage.BETA, StartupStage.GROWING, StartupStage.GROWTH, StartupStage.IDEA, StartupStage.MVP]
   }
 
   onNoClick(): void {
     this.dialogRef.close();
+  }
+
+  onClose(){
+    this.dialogRef.close()
+  }
+
+  get investorType(){
+    return this.form.get('investorType')
+  }
+
+  get name(){
+    return this.form.get('name')
+  }
+
+  get contact(){
+    return this.form.get('contact')
+  }
+
+  get linkedInLink(){
+    return this.form.get('linkedInLink')
+  }
+
+  get startupStage(){
+    return this.form.get('startupStage')
+  }
+
+  createList(){
+    this.dialogRef.close()
+    this.dataService.createFundList(this.form.value)
   }
 
 }
