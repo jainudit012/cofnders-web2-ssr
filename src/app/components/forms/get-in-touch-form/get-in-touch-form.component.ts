9dad9b67d8b2e9a981b1344f1322ef83c8e7fee1
jainudit012 <jainudit.012@gmail.com>
2019-07-15 15:57:25 +0530

    get in touch form component

--- src/app/components/forms/get-in-touch-form/get-in-touch-form.component.ts
+++ src/app/components/forms/get-in-touch-form/get-in-touch-form.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Optional, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { UserType } from '../../../models/user.model';
+import { DataService } from '../../../services/data.service';
 
 @Component({
   selector: 'app-get-in-touch-form',
@@ -11,14 +14,45 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class GetInTouchFormComponent implements OnInit {
 
+  userTypes: string[]
+
+  form = new FormGroup({
+    userType: new FormControl('', Validators.required),
+    linkedInLink: new FormControl('', Validators.required),
+    message : new FormControl('', Validators.maxLength(300))
+  })
+
   constructor(public dialogRef: MatDialogRef<GetInTouchFormComponent>,
-    @Optional() @Inject(MAT_DIALOG_DATA) private data: any) { }
+    @Optional() @Inject(MAT_DIALOG_DATA) private data: any,
+    public dataService: DataService) { }
 
   ngOnInit() {
+    this.userTypes = [UserType.Consulting_Firm, UserType.IT_Services, UserType.Investment_Advisor, UserType.Investor, UserType.Mentor, UserType.Startup_Founder]
   }
 
   onNoClick(): void {
-    this.dialogRef.close();
+    this.dialogRef.close()
+  }
+
+  onClose(){
+    this.dialogRef.close()
+  }
+
+  get userType(){
+    return this.form.get('userType')
+  }
+
+  get linkedInLink(){
+    return this.form.get('linkedInLink')
+  }
+
+  get message(){
+    return this.form.get('message')
+  }
+
+  createGetInTouch() {
+    this.dialogRef.close()
+    this.dataService.createGetInTouch(this.form.value, this.data)
   }
 
 }
