3d3868e1287b78234be6ed7282258d4f92b23805
jainudit012 <jainudit.012@gmail.com>
2019-07-16 18:09:41 +0530

    add delete opportunity

--- src/app/components/ideas/opportunity-card/opportunity-card.component.ts
+++ src/app/components/ideas/opportunity-card/opportunity-card.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 import { GetInTouchFormComponent } from '../../forms/get-in-touch-form/get-in-touch-form.component';
 import { WINDOW } from '@ng-toolkit/universal';
+import { ConfirmDeleteComponent } from '../../confirm-delete/confirm-delete.component';
 
 @Component({
   selector: 'opportunity-card',
@@ -34,6 +35,20 @@ export class OpportunityCardComponent implements OnInit {
       }else {
         this.authService.login(this.router.url)
       }
+  }
+
+  edit(){
+    console.log('edit opportunity')
+
+  }
+
+  delete(){
+    let dialogRef = this.dialog.open(ConfirmDeleteComponent, {
+      width: '30rem',
+      height: 'auto',
+      panelClass: 'dialog-form-pane',
+      data: {opportunity: this.opportunityData}
+    })
   }
 
 }
