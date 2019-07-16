5cd6145a5dcfffbf0b48e95fc4814418b4f5bd5f
jainudit012 <jainudit.012@gmail.com>
2019-07-16 15:25:53 +0530

    add data from dialog ref and add noapprvdprojcomp

--- src/app/components/funds/fund-card/fund-card.component.ts
+++ src/app/components/funds/fund-card/fund-card.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 import { ApplicationFormComponent } from '../../forms/application-form/application-form.component';
 import { WINDOW } from '@ng-toolkit/universal';
+import { NoApprovedProjectsComponent } from '../../no-approved-projects/no-approved-projects.component';
 
 @Component({
   selector: 'fund-card',
@@ -13,6 +14,7 @@ import { WINDOW } from '@ng-toolkit/universal';
 export class FundCardComponent implements OnInit {
 
   @Input('fund') fundData
+  @Input('projectData') approvedProjects:any[]
 
   constructor(public authService: AuthService,
     private router: Router,
@@ -25,12 +27,21 @@ export class FundCardComponent implements OnInit {
 
   apply() {
     if(this.authService.isAuthenticated() && this.authService.isUserAuthenticated()){
-      let dialogRef = this.dialog.open(ApplicationFormComponent, {
-        width: '40rem',
-        height: 'auto',
-        panelClass: "dialog-form-pane",
-        data: {id: this.fundData._id}
-      })
+      if(this.approvedProjects.length===0){
+        let dialogRef = this.dialog.open(NoApprovedProjectsComponent, {
+          width: '40rem',
+          height: 'auto',
+          panelClass: "dialog-form-pane",
+          data: {}
+        })
+      }else{
+        let dialogRef = this.dialog.open(ApplicationFormComponent, {
+          width: '40rem',
+          height: 'auto',
+          panelClass: "dialog-form-pane",
+          data: {id: this.fundData._id, projects: this.approvedProjects}
+        })
+      }
     }else {
       this.authService.login(this.router.url)
     }
