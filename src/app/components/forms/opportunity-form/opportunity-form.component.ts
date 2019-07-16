c9d856cb99e402f06fdaac4961f5a5da2890d797
jainudit012 <jainudit.012@gmail.com>
2019-07-16 15:24:57 +0530

    remove animation and add data from dialog ref

--- src/app/components/forms/opportunity-form/opportunity-form.component.ts
+++ src/app/components/forms/opportunity-form/opportunity-form.component.ts
@@ -3,14 +3,6 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Requirement } from '../../../models/opportunity.model';
 import { DataService } from '../../../services/data.service';
-import { HostBinding } from '@angular/core';
-import {
-  trigger,
-  state,
-  style,
-  animate,
-  transition,
-} from '@angular/animations';
 
 @Component({
   selector: 'app-opportunity-form',
@@ -18,24 +10,11 @@ import {
   styleUrls: ['./opportunity-form.component.scss'],
   providers: [
     // { provide: MAT_DIALOG_DATA, useValue: {user: {name: 'Udit Jain.'}, token: 'dwqoidowdouwoduweoud'} },
-  ],
-  animations:[
-    trigger('myInsertRemoveTrigger', [
-      transition(':enter', [
-        style({ opacity: 0 }),
-        animate('300ms', style({ opacity: 1 })),
-      ]),
-      transition(':leave', [
-        animate('300ms', style({ opacity: 0 }))
-      ])
-    ])
   ]
 })
 export class OpportunityFormComponent implements OnInit {
 
   requirementData: string[]
-  myProjectResponse: any
-  allUserProjects: any[] = []
   approvedProjects: any[]
 
   form = new FormGroup({
@@ -50,11 +29,7 @@ export class OpportunityFormComponent implements OnInit {
 
   async ngOnInit() {
     this.requirementData = [Requirement.CO_FOUNDER, Requirement.FUNDING, Requirement.IDEA_VALIDATION, Requirement.MENTORS_CONSULTANTS, Requirement.REFERENCE_CONNECTIONS, Requirement.SERVICE_PARTNERS_VENDORS]
-    this.myProjectResponse = await this.dataService.getMyProjects()
-    this.allUserProjects = [...this.myProjectResponse.projects]
-    this.approvedProjects = this.allUserProjects.filter(p=>{
-      return p.isApproved
-    })
+    this.approvedProjects = this.data.projects
   }
   
   onNoClick(): void {
