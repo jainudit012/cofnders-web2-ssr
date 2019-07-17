f841d0e6749168e3d648a4d05b4327a44dbc7b12
jainudit012 <jainudit.012@gmail.com>
2019-07-17 13:33:30 +0530

    add validation on details, fix form for edit

--- src/app/components/forms/opportunity-form/opportunity-form.component.ts
+++ src/app/components/forms/opportunity-form/opportunity-form.component.ts
@@ -16,11 +16,12 @@ export class OpportunityFormComponent implements OnInit {
 
   requirementData: string[]
   approvedProjects: any[]
+  isEdit:boolean = false
 
   form = new FormGroup({
     projectId: new FormControl('', Validators.required),
     requirement: new FormControl('', Validators.required),
-    details: new FormControl('', Validators.maxLength(300))
+    details: new FormControl('', [Validators.required, Validators.maxLength(300)])
   })
 
   constructor(public dialogRef: MatDialogRef<OpportunityFormComponent>,
@@ -30,6 +31,42 @@ export class OpportunityFormComponent implements OnInit {
   async ngOnInit() {
     this.requirementData = [Requirement.CO_FOUNDER, Requirement.FUNDING, Requirement.IDEA_VALIDATION, Requirement.MENTORS_CONSULTANTS, Requirement.REFERENCE_CONNECTIONS, Requirement.SERVICE_PARTNERS_VENDORS]
     this.approvedProjects = this.data.projects
+
+    if(this.data.opportunity){
+      let userProjectResponse = await this.dataService.getMyProjects()
+      let userProjects = [...userProjectResponse.projects]
+      let userApprovedProjects = userProjects.filter(p=>{
+        return p.isApproved
+      })
+      this.approvedProjects = userApprovedProjects
+      this.isEdit = true
+      this.setFormValue()
+    }
+  }
+
+  setFormValue() {
+    Object.keys(this.form.controls).forEach(field => {
+      const temp = {};
+      temp[field] = this.assignDefaultValue(this.form, field);
+      this.form.patchValue(temp)
+    });
+  }
+
+  assignDefaultValue(formGroup: FormGroup, field) {
+    let responseData = '';
+    switch (field) {
+      case 'projectId':
+        break;
+      case 'requirement':
+        responseData = Requirement[this.data.opportunity.requirement];
+        break;
+      case 'details':
+        responseData = this.data.opportunity.details;
+        break;
+      default:
+        responseData = ''
+    }
+    return responseData;
   }
   
   onNoClick(): void {
@@ -53,8 +90,12 @@ export class OpportunityFormComponent implements OnInit {
   }
 
   createOpportunity(){
+    if(this.data.opportunity){
+      this.dataService.editOpportunity(this.form.value, this.data.opportunity._id)
+    }else {
+      this.dataService.createOpportunity(this.form.value)
+    }
     this.dialogRef.close()
-    this.dataService.createOpportunity(this.form.value)
   }
   
   
