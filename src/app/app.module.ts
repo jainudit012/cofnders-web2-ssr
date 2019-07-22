de4b98728354a02756ac985538c2cc87f8efb5c4
jainudit012 <jainudit.012@gmail.com>
2019-07-22 12:04:03 +0530

    remove extra imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,54 +1,53 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { CloudinaryConfiguration, CloudinaryModule } from '@cloudinary/angular-5.x';
 import { NgtUniversalModule } from '@ng-toolkit/universal';
-import { CloudinaryModule, CloudinaryConfiguration } from '@cloudinary/angular-5.x';
 import { Cloudinary } from 'cloudinary-core';
 import { FileUploadModule } from 'ng2-file-upload';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutPageComponent } from './components/about-page/about-page.component';
 import { CallbackComponent } from './components/callback/callback.component';
+import { ConfirmDeleteComponent } from './components/confirm-delete/confirm-delete.component';
 import { CtaComponent } from './components/cta/cta.component';
 import { FilterComponent } from './components/filter/filter.component';
+import { FooterBlackComponent } from './components/footer-black/footer-black.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ApplicationFormComponent } from './components/forms/application-form/application-form.component';
 import { GetInTouchFormComponent } from './components/forms/get-in-touch-form/get-in-touch-form.component';
+import { ListFundFormComponent } from './components/forms/list-fund-form/list-fund-form.component';
+import { OpportunityFormComponent } from './components/forms/opportunity-form/opportunity-form.component';
 import { PostSignUpFormComponent } from './components/forms/post-sign-up-form/post-sign-up-form.component';
+import { ProjectFormComponent } from './components/forms/project-form/project-form.component';
 import { FundCardComponent } from './components/funds/fund-card/fund-card.component';
 import { FundComponent } from './components/funds/fund/fund.component';
-import { ListFundFormComponent } from './components/forms/list-fund-form/list-fund-form.component';
 import { IdeaComponent } from './components/ideas/idea/idea.component';
 import { OpportunityCardComponent } from './components/ideas/opportunity-card/opportunity-card.component';
-import { OpportunityFormComponent } from './components/forms/opportunity-form/opportunity-form.component';
 import { ProjectCardComponent } from './components/ideas/project-card/project-card.component';
-import { ProjectFormComponent } from './components/forms/project-form/project-form.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { NoApprovedProjectsComponent } from './components/no-approved-projects/no-approved-projects.component';
 import { PrivacyPageComponent } from './components/privacy-page/privacy-page.component';
 import { ProfileCardComponent } from './components/profile-card/profile-card.component';
 import { RecentCardComponent } from './components/recent-card/recent-card.component';
 import { TermsPageComponent } from './components/terms-page/terms-page.component';
 import { TrustPageComponent } from './components/trust-page/trust-page.component';
 import { MaterialModule } from './material.module';
-import { SubmitIfValidDirective } from './submit-if-valid.directive';
-import { MatDialogRef } from '@angular/material/dialog';
-import { FooterBlackComponent } from './components/footer-black/footer-black.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
 import { CustomDatePipe } from './pipes/customDate.pipe';
-import { TeamSizePipe } from './pipes/teamSize.pipe';
+import { InvestorTypePipe } from './pipes/investorType.pipe';
+import { OpportunityRequirementPipe } from './pipes/opportunityRequirement.pipe';
 import { ProjectCategoryPipe } from './pipes/projectCategory.pipe';
 import { ProjectSectorPipe } from './pipes/projectSector.pipe';
 import { ProjectStagePipe } from './pipes/projectStage.pipe';
-import { OpportunityRequirementPipe } from './pipes/opportunityRequirement.pipe';
-import { InvestorTypePipe } from './pipes/investorType.pipe';
 import { RemoveQuotesPipe } from './pipes/removeQuotes.pipe';
-import { NoApprovedProjectsComponent } from './components/no-approved-projects/no-approved-projects.component';
-import { ConfirmDeleteComponent } from './components/confirm-delete/confirm-delete.component';
+import { TeamSizePipe } from './pipes/teamSize.pipe';
+import { SubmitIfValidDirective } from './submit-if-valid.directive';
 
 @NgModule({
   declarations: [
@@ -104,6 +103,7 @@ import { ConfirmDeleteComponent } from './components/confirm-delete/confirm-dele
     HttpClientModule,
     MaterialModule,
     BrowserAnimationsModule,
+    FormsModule,
     ReactiveFormsModule,
     NgtUniversalModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
     CloudinaryModule.forRoot({Cloudinary}, { cloud_name: 'cofnderscloud' } as CloudinaryConfiguration),
