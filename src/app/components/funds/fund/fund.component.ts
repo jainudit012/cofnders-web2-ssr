9e0abc096eb1497af584982881260dfc010cdd6d
jainudit012 <jainudit.012@gmail.com>
2019-07-22 18:07:31 +0530

    change getting of data

--- src/app/components/funds/fund/fund.component.ts
+++ src/app/components/funds/fund/fund.component.ts
@@ -1,28 +1,43 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { DataService } from '../../../services/data.service';
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
+import { WINDOW } from '@ng-toolkit/universal';
+import { Subscription } from 'rxjs';
+
+import { FundValue, InvestorType } from '../../../models/fund.model';
+import { StartupStage } from '../../../models/project.model';
 import { AuthService } from '../../../services/auth.service';
+import { DataService } from '../../../services/data.service';
+import { FilterService } from '../../../services/filter.service';
+
 
 @Component({
   selector: 'app-fund',
   templateUrl: './fund.component.html',
   styleUrls: ['./fund.component.scss']
 })
-export class FundComponent implements OnInit {
+export class FundComponent implements OnInit, OnDestroy {
 
-  userProjectResponse: any
-  userProjects: any[]
-  userApprovedProjects:any[]
-  fundData:any
-  offset: number = 0
+  userProjectResponse: any;
+  userProjects: any[];
+  userApprovedProjects:any[];
+  fundData:any [];
+  fundBackup: any[];
+  offset: number = 0;
   doNextCallback: boolean = true;
+  filterSubscription: Subscription;
+  investorType: InvestorType | string
+  stage: StartupStage | string
+  fundValue: FundValue | string
+  type: string
 
   constructor(private dataService: DataService,
-    private authService: AuthService) { }
+    private authService: AuthService,
+    @Inject(WINDOW) private window: Window,
+    private filterService: FilterService) {
+    }
 
   async ngOnInit() {
-    this.dataService.getFunds().then(data=>{
-      this.fundData = data.funds
-    })
+    this.initData()
+    this.listenToValueChange()
 
     if(this.authService.isAuthenticated()&&this.authService.isUserAuthenticated()){
       this.userProjectResponse = await this.dataService.getMyProjects()
@@ -33,11 +48,17 @@ export class FundComponent implements OnInit {
     }
   }
 
+  initData(){
+    this.dataService.getFunds(this.investorType, this.stage, this.fundValue, this.type).then(data=>{
+      this.fundData = data.funds
+    })
+  }
+
   onScrollDown(){
     if(this.fundData.length < 10) this.doNextCallback = false;
     if(this.doNextCallback){
       this.offset += 10
-      this.dataService.getFunds(this.offset).then(data=>{
+      this.dataService.getFunds(this.investorType, this.stage, this.fundValue, this.type, this.offset).then(data=>{
         if(data.length < 10) this.doNextCallback = false
         let i
         for(i=0;i<data.feed.length;i++){
@@ -45,6 +66,49 @@ export class FundComponent implements OnInit {
         }
       })
     }
+  }
+
+  scrollToTop(){
+    this.window.scroll(0,0)
+  }
+
+  listenToValueChange(){
+    this.filterSubscription = this.filterService.generalData.subscribe(async(data)=>{
+      
+      if(data[0]&&data[0].length>0){
+        if(this.authService.isAuthenticated()&&this.authService.isUserAuthenticated()){
+          if(data[0]==='appliedFunds') this.type = 'applied'
+          this.initData()
+        }
+      }
+
+      if(data[1]&&data[1].length>1){
+        this.investorType = Object.keys(InvestorType).find(k=> InvestorType[k]===data[1])
+        this.initData()
+      }
+
+      if(data[2]&&data[2].length>0){
+        this.stage = Object.keys(StartupStage).find(k=> StartupStage[k]===data[2])
+        this.initData()
+      }
+
+      if(data[3]&&data[3].length>0){
+        this.fundValue = Object.keys(FundValue).find(k=> FundValue[k]===data[3])
+        this.initData()
+      }
+
+      if(!data[0]&&!data[1]&&!data[2]&&!data[3]){
+        this.investorType = ''
+        this.stage = ''
+        this.fundValue = ''
+        this.type = ''
+        this.initData()
+      }
+    })
+  }
+
+  ngOnDestroy(){
+    this.filterSubscription.unsubscribe()
   }
 
 }
