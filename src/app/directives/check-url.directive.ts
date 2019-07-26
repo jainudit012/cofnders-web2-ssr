ce9d2cd8f7b6499259e236d863b11d14b349730e
jainudit012 <jainudit.012@gmail.com>
2019-07-26 20:13:24 +0530

    add try catch for errors

--- src/app/directives/check-url.directive.ts
+++ src/app/directives/check-url.directive.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Directive } from '@angular/core';
 import { AbstractControl, AsyncValidator, NG_ASYNC_VALIDATORS, ValidationErrors } from '@angular/forms';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Directive({
@@ -14,20 +14,20 @@ export class CheckUrlDirective implements AsyncValidator {
 
   validate(control: AbstractControl): Observable<ValidationErrors|null> {
     let url = control.value
-    console.log(control.value.indexOf('https://'))
-    console.log(control.value.indexOf('http://'))
+    if(url.length===0) return of(null)
     if(control.value.indexOf('https://') === -1 && control.value.indexOf('http://') === -1){
       url = 'https://' + control.value  
     }else url = control.value  
-    console.log(url)
-    console.log(control.value)
-
-    return this.http.get(`https://helloacm.com/api/can-visit/?url=${url}`).pipe(
-      map((res:any)=> {
-        console.log(res)
-        if(res.code===200) return null
-        return ({'appCheckUrl': {value: control.value}})
-      })
-    )
+    try{
+      return this.http.get(`https://helloacm.com/api/can-visit/?url=${url}`).pipe(
+        map((res:any)=> {
+          if(res.code===200) return null
+          return ({'appCheckUrl': {value: control.value}})
+        })
+      )
+    }catch(err){
+      return of({'appCheckUrl': {value: control.value}})
+    }
+    
   }
 }
